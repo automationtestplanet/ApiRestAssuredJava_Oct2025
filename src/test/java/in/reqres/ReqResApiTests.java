86f9b33e9ee6b95b81d87b6d138d39656db46483
Automation Test Planet <automationtesttrainings@gmail.com>
2025-12-09 09:25:29 +0530

    RestAssured initial Commit

---
+++ src/test/java/in/reqres/ReqResApiTests.java
@@ -0,0 +1,98 @@
+package in.reqres;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class ReqResApiTests {
+
+
+    @Test
+    public void getListOfUsersTest() {
+        Response response = RestAssured.given().queryParam("page", 2).header("x-api-key", "reqres_c4dd8dedafed4c89ba31edf4eb997630")
+                .when().get("https://reqres.in/api/users").then().statusCode(200).extract().response();
+        response.prettyPrint();
+        Assert.assertEquals(response.getStatusCode(), 200, "Validate Expected Status code");
+        Assert.assertEquals(Integer.parseInt(response.jsonPath().get("page").toString()), 2, "Validate PageNum");
+        Assert.assertEquals( Integer.parseInt(response.jsonPath().get("data[0].id").toString()),7, "Validate First Record Data Id");
+
+        List<Map<String,Object>> dataList= response.jsonPath().getList("data");
+        System.out.println(dataList);
+
+        dataList.forEach(eachMap-> {
+            Assert.assertNotNull(eachMap.get("id"),"Each Data id should not be null");
+        });
+    }
+
+    @Test
+    public void createUserTest() {
+        String requestBOdy = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+
+        Response response = RestAssured.given().header("x-api-key", "reqres_c4dd8dedafed4c89ba31edf4eb997630")
+                .header("Accept","*/*")
+                .header("Content-Type","application/json")
+//                .header(ContentType.JSON)
+                .body(requestBOdy)
+                .when().post("https://reqres.in/api/users").then().statusCode(201).extract().response();
+        response.prettyPrint();
+        Assert.assertEquals(response.jsonPath().get("name"), "morpheus", "Validate Name");
+        Assert.assertEquals(response.jsonPath().get("job"), "leader", "Validate Job");
+        Assert.assertNotNull(response.jsonPath().get("id"),"Id should not be null");
+    }
+
+    @Test
+    public void updateUserTest() {
+        String requestBOdy = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"Automation Tester\"\n" +
+                "}";
+
+        Response response = RestAssured.given()
+                .pathParam("id",2)
+                .header("x-api-key", "reqres_c4dd8dedafed4c89ba31edf4eb997630")
+                .header("Accept","*/*")
+                .header("Content-Type","application/json")
+                .body(requestBOdy)
+                .when().put("https://reqres.in/api/users/{id}").then().statusCode(200).extract().response();
+        response.prettyPrint();
+        Assert.assertEquals(response.jsonPath().get("name"), "morpheus", "Validate Name");
+        Assert.assertEquals(response.jsonPath().get("job"), "Automation Tester", "Validate Job");
+    }
+
+    @Test
+    public void partialUpdateUserTest() {
+        String requestBOdy = "{\n" +
+                "    \"job\": \"QA Engineer\"\n" +
+                "}";
+
+        Response response = RestAssured.given()
+                .pathParam("id",2)
+                .header("x-api-key", "reqres_c4dd8dedafed4c89ba31edf4eb997630")
+                .header("Accept","*/*")
+                .header("Content-Type","application/json")
+                .body(requestBOdy)
+                .when().patch("https://reqres.in/api/users/{id}").then().statusCode(200).extract().response();
+
+        response.prettyPrint();
+
+        Assert.assertEquals(response.jsonPath().get("job"), "QA Engineer", "Validate Job");
+    }
+
+    @Test
+    public void deleteUserTest() {
+        Response response = RestAssured.given()
+                .pathParam("id",2)
+                .header("x-api-key", "reqres_c4dd8dedafed4c89ba31edf4eb997630")
+                .when().delete("https://reqres.in/api/users/{id}").then().statusCode(204).extract().response();
+
+        Assert.assertEquals(response.asString(),"","Response must be empty");
+    }
+}
